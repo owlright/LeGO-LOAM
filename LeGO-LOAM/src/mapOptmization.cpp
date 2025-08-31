b6b7e63984826c7b1b1c2485423c8f982321aea6
owlright <jhsheng@mail.ustc.edu.cn>
2025-08-31 20:26:20 +0800

    尝试用Eigen::Affine3f替代原始数组

--- LeGO-LOAM/src/mapOptmization.cpp
+++ LeGO-LOAM/src/mapOptmization.cpp
@@ -176,6 +176,13 @@ private:
     float transformTobeMapped[6];
     float transformBefMapped[6];
     float transformAftMapped[6];
+    Eigen::Affine3f translast;
+    Eigen::Affine3f transSum;
+    Eigen::Affine3f transIncre;
+    Eigen::Affine3f transTobeMapped;
+    Eigen::Affine3f transBefMapped;
+    Eigen::Affine3f transAftMapped;
+
 
 
     int imuPointerFront;
